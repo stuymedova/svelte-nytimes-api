155cb310114805de27a292454d2f8f01b9bdeea3
Sofya Tuymedova <stuymedova@Sofyas-MacBook-Air.local>
2021-02-11 02:54:55 +0300

    nyt api & edits

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,28 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+
+	let articles = [];
+	let results = [];
+
+	onMount(async () => {
+		const res = await fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=`);
+		articles = await res.json();
+		// (line below) added due to the structure of an api
+		results = articles.results;
+	});
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>NYT Top Stories</h1>
+  <ul>
+		{#each results as article}
+			<li>
+				<a href={article.short_url}><h2>{article.title}</h2></a>
+				<p>{article.byline}</p>
+				<p>{article.abstract}</p>
+			</li>
+		{:else}
+			<p>loading...</p>
+		{/each}
+	</ul>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
